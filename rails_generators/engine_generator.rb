d15000b09a251e8969a32f956acafef3e2ef5b1c
amardaxini <amardaxini@gmail.com>
2009-07-23 16:28:11 +0530

    update engine generator refactored code and application controller changed

--- rails_generators/engine_generator.rb
+++ rails_generators/engine_generator.rb
@@ -1,4 +1,5 @@
 class EngineGenerator < Rails::Generator::NamedBase
+ 
   attr_accessor :plugin_path,:source_dir,:plugin_name,:db_name,:user_name,:password
   require 'fileutils'
   
@@ -65,11 +66,44 @@ class EngineGenerator < Rails::Generator::NamedBase
    def constant_name
      file_name.underscore.upcase
    end
+  def singular_name
+    file_name.underscore
+  end
+  
+  def plural_name
+   file_name.underscore.pluralize
+  end
+  
+  def class_name
+   file_name.camelize
+  end
+  
+  def plural_class_name
+    plural_name.camelize
+  end
   
    def after_generate
      # `rake enginedb:create`
      #add filename =amar then add amar_dev to an all model  self.establish_connection :amar_dev 
-     Dir.glob("#{plugin_path}/app/models/*.rb").each do |f|
+     change_model_content
+     # add to database yml amar_dev with data base amar_dev
+     change_database_yml_content
+    
+     #added self.connection modelname.connection end
+    change_migration_content
+      
+      #replace "< ApplicationController" with " < plural_class_name" 
+      change_controller_content 
+     
+      # now Change Plugin ApplicationController to #plural_class_name and inhrit from main ApplicationController
+      #prepend line require 'application_controller.rb'
+      change_application_controller_of_engine
+      
+      #Added all migration to base migration      
+      FileUtils.cp_r(@plugin_path+"/db/migrate/.",'db/migrate')
+   end
+   def change_model_content
+    Dir.glob("#{plugin_path}/app/models/*.rb").each do |f|
        content = ""
        file = File.open(f,"r")
        file.each do |line|
@@ -77,18 +111,22 @@ class EngineGenerator < Rails::Generator::NamedBase
        end
        db_connection = "self.establish_connection :#{file_name}_dev"
        content.sub!(/ActiveRecord::Base\s{1}/,"ActiveRecord::Base\n #{db_connection} \n")
+       
        #Find out ActiveRecord::Base and after this line place self.establish_connection :amar_dev 
        file = File.open(f,"w")
        file.write(content)
        file.close
      end
-     # add to database yml amar_dev with data base amar_dev
+   end
+    # add to database yml #{file_name}_dev with data base #{file_name}_dev
+   def change_database_yml_content
      dbconfig = YAML::load(File.open('config/database.yml'))
      mydb = {"#{file_name}_dev"=>{"encoding"=>"utf8","reconnect"=>false,"adapter"=>"mysql","username"=>"root", "database"=>"#{file_name}_dev","pool"=>5, "password"=>"", "socket"=>"/var/run/mysqld/mysqld.sock"}}
      dbconfig =  dbconfig.merge(mydb)
      File.open('config/database.yml','w'){ |f| YAML.dump(dbconfig, f) }
-    
-     #added self.connection modelname.connection end
+   end
+       #added self.connection modelname.connection end
+   def change_migration_content
      Dir.glob("#{plugin_path}/db/migrate/*.rb").each do |f|
        content = ""
        file = File.open(f,"r")
@@ -109,17 +147,50 @@ class EngineGenerator < Rails::Generator::NamedBase
         migrate_connection = "def self.connection
               #{model_name.singularize}.connection
               end"
-        end
-        if !model_name.blank?
-          content.sub!(/ActiveRecord::Migration\s{1}/,"ActiveRecord::Migration\n #{migrate_connection} \n")
-        end
-        file = File.open(f,"w")
-        file.write(content)
-        file.close
       end
-      #Added all migration to base migration      
-      FileUtils.cp_r(@plugin_path+"/db/migrate/.",'db/migrate')
+      if !model_name.blank?
+        content.sub!(/ActiveRecord::Migration\s{1}/,"ActiveRecord::Migration\n #{migrate_connection} \n")
+      end
+      file = File.open(f,"w")
+      file.write(content)
+      file.close
+    end
+    
    end
+    #replace "< ApplicationController" with " < plural_class_nameController" 
+   def change_controller_content
+
+     Dir.glob("#{plugin_path}/app/controllers/*.rb").each do |f|
+       content = ""
+       file = File.open(f,"r")
+       file.each do |line|
+         content += "#{line} " 
+       end
+       content.sub!(/< ApplicationController{1}/,"< #{plural_class_name}Controller \n")
+       file = File.open(f,"w")
+       file.write(content)
+       file.close
+     end 
+   end
+   # now Change Plugin ApplicationController to #plural_class_name and inhrit from main ApplicationController
+   #prepend line require 'application_controller.rb'
+   def change_application_controller_of_engine
+     
+      application_content = ""
+      application_controller_file = File.open("#{plugin_path}/app/controllers/application_controller.rb","r")
+      application_controller_file .each do |line|
+        application_content += "#{line} " 
+      end
+      app1 = "require"
+      app2 ="'application_controller.rb'"
+      app3 = "class #{plural_class_name}Controller < ApplicationController"
+      application_content.sub!(/class ApplicationController < ActionController::Base{1}/,"#{app1}\s#{app2} \n#{app3}")
+      application_controller_file = File.open("#{plugin_path}/app/controllers/application_controller.rb","w")
+      application_controller_file.write(application_content)
+      application_controller_file.close
+      File.rename("#{plugin_path}/app/controllers/application_controller.rb","#{plugin_path}/app/controllers/#{plural_name}_controller.rb")
+   end
+   
    
    
 end
